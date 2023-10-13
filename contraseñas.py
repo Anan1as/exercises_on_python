53202afcf84ff609cb4fff7f1900543480712be0
Ananias <99740075+Anan1as@users.noreply.github.com>
2023-10-13 16:01:45 -0400

    Add files via upload

---
+++ contraseñas.py
@@ -0,0 +1,13 @@
+
+
+'''
+listaNombres = []
+cantidadNombres = int(input("Ingrese la cantidad de nombres: "))
+for cantidadNombre in range(cantidadNombres):
+    nombre=input("Ingrese un nombre: ")
+    listaNombres.append(nombre)
+
+Crea un generador de contraseñas
+#Se deberá solicitar al usuario la cantidad de caracteres que desea que tenga su contraseña.
+#La contraseña debe contener letras y números al azar.
+'''
