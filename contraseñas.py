2417d57d430e02b155080d2b625b6431b3b13ec5
Ananias <amychelleoropezaortiz@gmail.com>
2023-10-13 16:34:49 -0400

    Acomodando la info :>

--- contraseñas.py
+++ contraseñas.py
@@ -5,10 +5,12 @@ def generarContraseña(largo):
     contraseña = ""
 
     for pop in range(largo):
+        #La contraseña debe contener letras y números al azar.
         contraseña += random.choice(caracteres)
     
     return contraseña
 
+#Se deberá solicitar al usuario la cantidad de caracteres que desea que tenga su contraseña.
 cantidadCaracteres = int(input("Ingrese que tan larga quiere que sea su contraseña: "))
 
 if cantidadCaracteres < 1:
